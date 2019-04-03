61bb57c4f2bc9f7ed59366fe634a5d33bdfc7449
FreshFighter <2215230698@qq.com>
2019-04-03 15:25:34 +0800

    第三次作业第一次提交

---
+++ 201731062523/wordCount1/wordCountClassLibrary/CountCharacters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordCountClassLibrary
+{
+    /// <summary>
+    /// 统计字符数的类
+    /// </summary>
+    public class CountCharacters
+    {
+        public string text;
+        public CountCharacters(string s)
+        {
+            this.text = s;
+        }
+        /// <summary>
+        /// 统计字符数的函数
+        /// </summary>
+        /// <returns>字符个数</returns>
+        public int Count_character()
+        {
+            if(this.text!=null)
+            {
+                int count = 0;
+                foreach (char ascii in this.text)
+                {
+                    if (ascii >= 0 && ascii <= 127)
+                        ++count;
+                }
+                Console.WriteLine("characters:" + count);
+                return count;
+            }
+            else
+            {
+                Console.WriteLine("文件为空");
+                return 0;
+            }
+        }
+    }
+}
---
+++ 201731062523/wordCount1/wordCountClassLibrary/CountLines.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordCountClassLibrary
+{
+    /// <summary>
+    /// 统计有效行数的类
+    /// </summary>
+    public class CountLines
+    {
+        public string text;
+        public CountLines(string s)
+        {
+            this.text = s;
+        }
+        /// <summary>
+        /// 统计有效行数的函数
+        /// </summary>
+        /// <returns>行数</returns>
+        public int Count_line()
+        {
+            if(this.text!=null)
+            {
+                string[] wordList = this.text.Split("\r\n".ToCharArray());
+                int count = 0;
+                foreach (string word in wordList)
+                    if (word.Length > 0)
+                        ++count;
+                Console.WriteLine("lines:" + count);
+                return count;
+            }
+            else
+            {
+                Console.WriteLine("文件为空");
+                return 0;
+            }
+        }
+    }
+}
